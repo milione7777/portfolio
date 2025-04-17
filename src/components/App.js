43bcc4afed8874402d1161b596ddeeb7b98d1aeb
Artem <vsezdes021@gmail.com>
2025-04-17 10:01:26 +0300

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import Tech from "./tech";
 function App() {
   
   return (
-   <div className="relative min-h-[170vh] inset-0 -z-10 h- w-screen [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+   <div className="relative min-h-[100vh] inset-0 -z-10 h- w-screen [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
       <Header />
       <Hero />
       <Tech />
